1b0186be05bb57fae0a657b9256df20a0742c07a
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2021-05-27 16:36:44 -0300

    create home

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:modular_app/app/pages/home/home_controller.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,25 +9,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final homeController = Modular.get<HomeController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
+      body: Observer(
+        builder: (_) {
+          if (homeController.pokemons.error != null) {
+            return Center(
+              child: Text('Erro'),
+            );
+          }
+          if (homeController.pokemons.value == null) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          var list = homeController.pokemons.value;
+
+          return ListView.builder(
+              itemCount: list.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(list[index].name),
+                );
+              });
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.pushNamed(context, '/other');
+          // Navigator.pushNamed(context, '/other/${homeController.text}');
+          Modular.to.pushNamed('/other');
         },
         child: Icon(Icons.add),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(
-            decoration: InputDecoration(labelText: 'Um texto qualquer'),
-          ),
-        ),
       ),
     );
   }
